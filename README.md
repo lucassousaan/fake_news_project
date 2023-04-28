22684e0990e80737f7c90de62845f50012ddb899
lucassousaan <lucas11.ptc@gmail.com>
2023-04-28 18:28:02 -0300

    Readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# fake_news_project
+﻿## Trabalho de Conclusão de Curso - Fake News Detector
+
+Código relacionado ao Trabalho de Conclusão de Curso de Lucas Sousa:
+
+  - Separa alguns arquivos de um dado dataset para um novo arquivo .csv
+  - Adiciona uma coluna com o label dessa notícia, falsa ou verdadeira
+  - Pergunta ao GPT se a notícia é verdadeira ou falsa, e adiciona mais uma coluna no arquivo
+  - Classifica a informação dada pelo GPT como VP, FP, VN ou FN
+  - Calcula: F1-Score, Recall, Acurácia, etc
+
+### Dependências
+
+Projeto está sendo realizado em Python:
+
+##### Principais Bibliotecas
+* [Python](https://www.python.org/) - Linguagem principal do app.
+
--- extract_data.py
+++ extract_data.py
@@ -16,30 +16,14 @@ with open("dataset/fake-news-data.csv", 'rt', encoding='utf8') as fileFake:
             break
     fileFake.close()
 
-with open("dataset/true-news-data.csv", 'rt', encoding='utf8') as fileFake:
-    readerFake = csv.reader(fileFake)
-    counterFake = 0
-    for rowFake in readerFake:
-        if counterFake > 0:
-            writerCsv.writerow([rowFake[0], rowFake[1], rowFake[2], "verdadeira"])
-        counterFake += 1
-        if counterFake == 101:
+with open("dataset/true-news-data.csv", 'rt', encoding='utf8') as fileTrue:
+    readerTrue = csv.reader(fileTrue)
+    counterTrue = 0
+    for rowTrue in readerTrue:
+        if counterTrue > 0:
+            writerCsv.writerow([rowTrue[0], rowTrue[1], rowTrue[2], "verdadeira"])
+        counterTrue += 1
+        if counterTrue == 101:
             break
-    fileFake.close()
-
-
-
-
-    
-
-# with open("dataset/true-news-data.csv", 'r') as fileTrue:
-#     readerTrue = csv.reader(fileTrue)
-#     counterTrue = 0
-#     for rowTrue in readerTrue:
-#         print(rowTrue)
-#         counterTrue += 1
-#         if counterTrue == 3:
-#             break
-#     fileTrue.close()
-
+    fileTrue.close()
     
